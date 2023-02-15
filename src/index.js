b725ff67d527f858a00ee1d3dde7665f953809b4
DaryaSolomennikova <solomka306@gmail.com>
2023-02-15 17:44:40 +0300

    add refactoring code

--- src/index.js
+++ src/index.js
@@ -2,7 +2,13 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
- return ( matrix.reduce((acc, cur, i) => {
+  if (matrix === undefined){
+    return [];
+} else {
+  return ( matrix.reduce((acc, cur, i) => {
     return acc.concat((!(i % 2) ? cur : cur.reverse()));
   }, []));
 }
+}
+
+
